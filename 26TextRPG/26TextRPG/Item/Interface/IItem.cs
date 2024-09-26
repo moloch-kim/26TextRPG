4fbe792e7bf9f9115c9ddd4d30379ae5fbd89750
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-09-26 20:04:38 +0900

    Item Interface, Stage Class

---
+++ 26TextRPG/26TextRPG/Item/Interface/IItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26TextRPG.Item.Interface
+{
+    internal interface IItem
+    {
+        int ID { get; }
+        string Name { get; }
+        string Description { get; }
+        int Value { get; }
+
+    }
+
+    
+
+}

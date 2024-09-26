0a5107bdc944dc40b35461fa27782b561e006634
moomin <amily8050@gmail.com>
2024-09-26 21:12:55 +0900

    Create Enemy.cs, Player.cs

--- 26TextRPG/26TextRPG/Character.cs
+++ 26TextRPG/26TextRPG/Character.cs
@@ -32,39 +32,3 @@ public class Character
 	}
 }
 
-
-
-public class Enemy
-{
-	public string Name { get; private set; }
-	public int Speed { get; private set; }
-	public int ActionGauge { get; private set; }
-
-	public Enemy(string name, int speed)
-	{
-		Name = name;
-		Speed = speed;
-		ActionGauge = 0;
-	}
-
-	public void ChargeEnemyActionGauge()
-	{
-		if (Health > 0) // 적이 살아있을 때만 게이지 충전
-		{
-			ActionGauge += Speed;
-			if (ActionGauge >= 100)
-				ActionGauge = 100;
-		}
-	}
-
-	public bool EnemyCanAct()
-	{
-		return ActionGauge >= 100;
-	}
-
-	public void ResetEnemyActionGauge()
-	{
-		ActionGauge = 0;
-	}
-}
-
---
+++ 26TextRPG/26TextRPG/Enemy.cs
@@ -0,0 +1,34 @@
+public class Enemy
+{
+	string name { get; set; }
+	int speed { get; set; }
+	int ActionGauge { get; set; }
+    int health { get; set; }
+
+	public Enemy(string _name, int _speed)
+	{
+		name = _name;
+		speed = _speed;
+		ActionGauge = 0;
+	}
+
+	public void ChargeEnemyActionGauge()
+	{
+		if (health > 0) // 적이 살아있을 때만 게이지 충전
+		{
+			ActionGauge += speed;
+			if (ActionGauge >= 100)
+				ActionGauge = 100;
+		}
+	}
+
+	public bool EnemyCanAct()
+	{
+		return ActionGauge >= 100;
+	}
+
+	public void ResetEnemyActionGauge()
+	{
+		ActionGauge = 0;
+	}
+}

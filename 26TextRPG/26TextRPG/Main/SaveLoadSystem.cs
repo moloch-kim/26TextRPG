2e5503bf9a85e4753d21bac9669c330643fed34f
geuneda <whdrmsvjzl@naver.com>
2024-09-28 01:00:12 +0900

    Refactor: Program.cs, SaveLoadSystem.cs, MainScene.cs
    사용자 데이터 저장 및 불러오기시스템을 위해 데이터를 매개변수가 아닌 객체로 받아오기위해 코드 대거 수정

--- 26TextRPG/26TextRPG/Main/SaveLoadSystem.cs
+++ 26TextRPG/26TextRPG/Main/SaveLoadSystem.cs
@@ -6,54 +6,32 @@ namespace _26TextRPG.Main
 
     public class SaveLoadSystem
     {
-        private static string filePath = "gameData.json";
-
-        public static void SaveGame(GameData data)
+        private static string filePath = "playerData.json";
+        static MainScene mainScene = new MainScene();
+        public static void SaveGame(Player player)
         {
-            // 데이터를 JSON형식으로 직렬화 하는 코드
-            //Formatting.Indented 이부분은 들여쓰기를 적용하는 "옵션"
-            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
-            //위에서 직렬화 한 JSON데이터를 파일로 저장
+            string json = JsonConvert.SerializeObject(player, Formatting.Indented);
             File.WriteAllText(filePath, json);
-            // 출력 및 슬립을 줘서 사용자가 저장중인것을 인지시킴
             Console.WriteLine("진행상황을 저장중입니다...");
             Thread.Sleep(3000);
         }
-        public static GameData LoadGame()
+        public static Player LoadGame()
         {
-            //데이터 파일이 있는지 확인하고
             if (File.Exists(filePath))
             {
-                //JSON데이터를 문자열로 변환
                 string json = File.ReadAllText(filePath);
-                //JSON형식의 문자열을 GameData객체로 변환해주는 코드 (압축과 압축풀기로 이해하면 매우 쉬움)
-                GameData data = JsonConvert.DeserializeObject<GameData>(json);
+                Player player = JsonConvert.DeserializeObject<Player>(json);
                 Console.WriteLine("진행상황을 불러오는 중입니다...");
                 Thread.Sleep(3000);
-                //불러온 변환한 데이터를 반환해서 게임내에 반영
-                return data;
+                return player;
             }
             else
             {
                 Console.WriteLine("불러올 파일이 없습니다.");
                 Thread.Sleep(3000);
                 return null;
+                mainScene.CreatePlayer();
             }
         }
-        public void Save()
-        {
-            // GameData객체를 생성하고 현재 가지고있는 저장해야할 변수들을 넣어줌
-            GameData gameData = new GameData
-            {
-                //저장할변수 = 저장할변수,
-                //level = level,
-                //exp = exp,
-                //gold = gold,
-            };
-            SaveLoadSystem.SaveGame(gameData);
-        }
-        
-
-
     }
 }
--- 26TextRPG/26TextRPG/Program.cs
+++ 26TextRPG/26TextRPG/Program.cs
@@ -4,34 +4,12 @@ namespace _26TextRPG
 {
     public class Program
     {
+        private Player currentPlayer;
         static void Main()
         {
-            Program program = new Program();
-            Player player = program.CreatePlayer();
             MainScene mainScene = new MainScene();
             mainScene.Opening();
-            mainScene.RunGame(player);
-        }
-
-        public Player CreatePlayer()
-        {
-            Console.WriteLine("게임에 처음 접속하셨습니다.");
-            Console.WriteLine("원하는 닉네임을 입력해주세요.");
-            string nickName = Console.ReadLine();
-            Player player = new Player(nickName);
-            Console.WriteLine($"닉네임 : {player.Name}");
-            Thread.Sleep(2000);
-            return player;
-        }
-    }
-
-    public class Player
-    {
-        public string Name { get; set; }
-
-        public Player(string name)
-        {
-            Name = name;
+            mainScene.RunGame();
         }
     }
 }

252c43dec03d4750b18ecd993d3a78e7bab73a19
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-09-27 10:03:16 +0900

    Ignore Dummy csFile
    참고용으로 작성하신 클래스 파일을 주석처리하여 충돌을 방지했습니다

--- 26TextRPG/26TextRPG/Player.cs
+++ 26TextRPG/26TextRPG/Player.cs
@@ -1,18 +1,18 @@
-public class Player
-{
-    int level { get; set; } = 1;
-    string name { get; set; }
-    string job { get; set; } = "전사";
-    int attack { get; set; } = 10;
-    int defense { get; set; } = 5;
-    int health { get; set; } = 100;
-    int gold { get; set; } = 1500;
-    int speed { get; set; }
+    public class Player
+    {
+        int level { get; set; } = 1;
+        string name { get; set; }
+        string job { get; set; } = "전사";
+        int attack { get; set; } = 10;
+        int defense { get; set; } = 5;
+        int health { get; set; } = 100;
+        int gold { get; set; } = 1500;
+        int speed { get; set; }
 
-	public Player(string _name, int _speed)
-	{
-		name = _name;
-		// speed = _speed;
-	}
+        public Player(string _name, int _speed)
+        {
+            name = _name;
+            // speed = _speed;
+        }
 
-}
+    }

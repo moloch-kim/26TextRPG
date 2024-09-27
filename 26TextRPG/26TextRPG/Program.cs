fa0697a87d726de58365ff89724f35e2c54c398a
geuneda <whdrmsvjzl@naver.com>
2024-09-28 00:06:17 +0900

    Simplify: Program.cs
    코드 정리

--- 26TextRPG/26TextRPG/Program.cs
+++ 26TextRPG/26TextRPG/Program.cs
@@ -4,22 +4,34 @@ namespace _26TextRPG
 {
     public class Program
     {
-
-        public void Stat()
-        {
-            
-        }
-
         static void Main()
         {
-            CreateAcc createAcc = new CreateAcc();
-            Player player = new Player(createAcc.nickName);
-            player.Name = createAcc.nickName;
-            Console.WriteLine($"닉네임 : {player.Name}");
+            Program program = new Program();
+            Player player = program.CreatePlayer();
             MainScene mainScene = new MainScene();
             mainScene.Opening();
             mainScene.RunGame(player);
-            //게임시작
+        }
+
+        public Player CreatePlayer()
+        {
+            Console.WriteLine("게임에 처음 접속하셨습니다.");
+            Console.WriteLine("원하는 닉네임을 입력해주세요.");
+            string nickName = Console.ReadLine();
+            Player player = new Player(nickName);
+            Console.WriteLine($"닉네임 : {player.Name}");
+            Thread.Sleep(2000);
+            return player;
+        }
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
         }
     }
 }

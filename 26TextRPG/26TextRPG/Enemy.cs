b799942173546f6ff4ef63417fb6bab78e75e765
moomin <amily8050@gmail.com>
2024-09-27 19:30:08 +0900

    Update Battle.cs, Enemy.sc, Player.cs

--- 26TextRPG/26TextRPG/Enemy.cs
+++ 26TextRPG/26TextRPG/Enemy.cs
@@ -1,33 +1,35 @@
-	public class Enemy: Character
+public class Enemy
+{
+	public string Name { get; }
+	public int ID { get; } 
+	public int Health { get; set; }
+	public int MaxHealth { get; }
+	public int AttackPower { get; }
+	public int DefensePower { get; }
+	public int Speed { get; }
+	public int ExperienceReward { get; }
+	public int GoldReward { get; }
+	public int ActionGauge { get; private set; } = 0;
+
+	public bool IsAlive()
 	{
-		public string Name { get; }
-		public int Health { get; set; }
-		public int MaxHealth { get; }
-		public int AttackPower { get; }
-		public int DefensePower { get; }
-		public int Speed { get; }
-		public int ExperienceReward { get; }
-		public int GoldReward { get; }
-		public int ActionGauge { get; private set; }
+		return Health > 0;
+	}
 
-		public bool IsAlive()
-		{
-			return Health > 0;
-		}
+	public Enemy(string name, int id, int health, int attackPower, int defensePower, int speed, int experienceReward, int goldReward)
+	{
+		Name = name;
+		ID = id;
+		Health = MaxHealth = health;
+		AttackPower = attackPower;
+		DefensePower = defensePower;
+		Speed = speed;
+		ExperienceReward = experienceReward;
+		GoldReward = goldReward;
+	}
 
-		public Enemy(string name, int health, int attackPower, int defensePower, int speed, int experienceReward, int goldReward)
-		{
-			Name = name;
-			Health = MaxHealth = health;
-			AttackPower = attackPower;
-			DefensePower = defensePower;
-			Speed = speed;
-			ExperienceReward = experienceReward;
-			GoldReward = goldReward;
-			ActionGauge = 0; // 초기화
-		}
-
-    public void ChargeEnemyActionGauge()
+	// ChargeEnemyActionGauge -> ChargeActionGauge로 함수명 수정(아래 함수들도 동일하게 Enemy 생략)
+    public void ChargeActionGauge()
 		{
 			if (Health > 0) // 적이 살아있을 때만 게이지 충전
 			{
@@ -37,21 +39,21 @@
 			}
 		}
 
-		public bool EnemyCanAct()
-		{
-			return ActionGauge >= 100;
-		}
+	public bool CanAct()
+	{
+		return ActionGauge >= 100;
+	}
 
-		public void ResetEnemyActionGauge()
-		{
-			ActionGauge = 0;
-		}
+	public void ResetActionGauge()
+	{
+		ActionGauge = 0;
+	}
 
-		public void Attack(Character player)
-		{
-			int damage = AttackPower - player.TotalDefensePower;
-			if (damage < 0) damage = 0;
-			player.Health -= damage;
-			Console.WriteLine($"{Name}이(가) {player.Name}에게 {damage}만큼의 피해를 입혔습니다.");
-		}
+	public void Attack(Player player)
+	{
+		int damage = AttackPower - player.TotalDefensePower;
+		if (damage < 0) damage = 0;
+		player.Health -= damage;
+		Console.WriteLine($"{Name}이(가) {player.Name}에게 {damage}만큼의 피해를 입혔습니다.");
+	}
 }

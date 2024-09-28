0779d9bc0145224b6efdb9b826e1067f20271bab
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-09-28 12:18:38 +0900

    minor update

--- 26TextRPG/26TextRPG/Dungeon/Shop.cs
+++ 26TextRPG/26TextRPG/Dungeon/Shop.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _26TextRPG.Item;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace _26TextRPG.Dungeon
 {
-    internal class Shop
+    public enum Shoplist
     {
+        Armor = 100, Consum = 200, Weapon = 300,
+        Startshop = 1
+    }
+    public class Shop
+    {
+        public List<_26TextRPG.Item.Item> ItemsForSale { get; set; }
+
+        //public Shop()
     }
 }

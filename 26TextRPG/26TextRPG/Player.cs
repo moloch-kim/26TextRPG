011ecc6b79e154be47703aeb88468109fdf0d126
moomin <amily8050@gmail.com>
2024-09-30 17:36:22 +0900

    Update Character.cs, Enemy.cs, Player.cs
    상속 구현

--- 26TextRPG/26TextRPG/Player.cs
+++ 26TextRPG/26TextRPG/Player.cs
@@ -2,46 +2,40 @@ using _26TextRPG;
 using _26TextRPG.Dungeon;
 using _26TextRPG.Main;
 using System.ComponentModel;
-public class Player
+public class Player : Character
 {
     private static Player instance;
     // 초기값 설정(상태 보기 화면에 뜨는)
-    public string Name { get; set; }
     public int Level { get; set; }
     public string Job { get; set; } 
-    public int AttackPower { get; set; }
-    public int TotalAttackPower { get; set; }
-    public int DefensePower { get; set; }
-    public int TotalDefensePower { get; set; }
-    public int Health { get; set; }
-    public int MaxHealth { get; set; }
+    public int BaseAttackPower { get; set; }
+    public int BaseDefensePower { get; set; }
     public int Gold { get; set; }
-    public int Speed { get; set; }
     public int Mana { get; set; }
     public int MaxMana { get; set; }
     public int Exp { get; set; }
-    public int ExpToNextLevel { get; set; }
+    public int ExpToNextLevel { get; set; } = 100;
     public int StatPoint;
-    public int ActionGauge { get; set; }
     public bool IsDefending { get; set; }
     public List<Skill> SkillList { get; } = new List<Skill>();
     public List<Item> Inventory { get; } = new List<Item>();
     public Quest Quest { get; set; }
-    public Player(string name, string job, int attackPower, int defensePower, int maxHealth, int speed, int maxMana, int gold)
+    public Player(string name, string job, int baseAttackPower, int baseDefensePower, int maxHealth, int speed, int maxMana, int gold)
     {
         Name = name;
         Job = job;
-        AttackPower = attackPower;
-        DefensePower = defensePower;
+        BaseAttackPower = baseAttackPower;
+        BaseDefensePower = baseDefensePower;
         MaxHealth = maxHealth;
         Health = maxHealth;
         Speed = speed;
         Mana = maxMana;
         MaxMana = maxMana;
         Gold = gold;
-        ExpToNextLevel = 100;
-        Inventory = new List<Item>();
-        SkillList = new List<Skill>();
+        // 유민) 변수 선언부분에서 이미 구현된 부분이라서 주석처리했습니다. 해당 코드 작성하신 분 이거 확인하시면 주석 제거해주세요!
+        // ExpToNextLevel = 100;
+        // Inventory = new List<Item>();
+        // SkillList = new List<Skill>();
     }
 
     public static Player Instance
@@ -57,49 +51,6 @@ public class Player
         instance = loadedPlayer;
     }
 
-    MainScene mainScene = new MainScene();
-
-    public void ChargeActionGauge()
-	{
-		ActionGauge += Speed;
-		if (ActionGauge >= 100)
-			ActionGauge = 100;
-	}
-
-    public bool CanAct()
-	{
-		return ActionGauge >= 100;
-	}
-
-    public void ResetActionGauge()
-	{
-		ActionGauge = 0;
-	}
-
-    public void Attack(Enemy enemy)
-	{
-        int AttackRoll = Dice.Roll(1, 20);
-        if (AttackRoll == 20)
-        {
-            int damage = TotalAttackPower - enemy.DefensePower;
-            if (damage < 0) damage = 0;
-            enemy.Health -= damage * 2;
-            mainScene.TypingEffect("정말 치명적인 일격입니다!!", 30);
-            mainScene.TypingEffect($"{Name}이(가) {enemy.Name}에게 {damage}만큼의 피해를 입혔습니다!", 50);
-        }
-        else if (AttackRoll == 1)
-        {
-            mainScene.TypingEffect("어이없는 실수로 공격이 빗나갑니다!!", 50);
-        }
-        else
-        {
-            int damage = TotalAttackPower - enemy.DefensePower;
-            if (damage < 0) damage = 0;
-            enemy.Health -= damage;
-            Console.WriteLine($"{Name}이(가) {enemy.Name}에게 {damage}만큼의 피해를 입혔습니다.");
-        }
-    }
-
     public void Defend()
     {
         IsDefending = true;
@@ -110,7 +61,7 @@ public class Player
 	{
 
 		Mana -= skill.ManaCost;
-		int damage = (int)(TotalAttackPower * skill.Multiplier) - enemy.DefensePower;
+		int damage = (int)(AttackPower * skill.Multiplier) - enemy.DefensePower;
 		if (damage < 0) damage = 0;
 		enemy.Health -= damage;
 
@@ -154,11 +105,11 @@ public class Player
             switch (choice)
             {
                 case "1":
-                    AttackPower++;
+                    BaseAttackPower++;
                     Console.WriteLine("공격력이 증가했습니다.");
                     break;
                 case "2":
-                    DefensePower++;
+                    BaseDefensePower++;
                     Console.WriteLine("방어력이 증가했습니다.");
                     break;
                 case "3":

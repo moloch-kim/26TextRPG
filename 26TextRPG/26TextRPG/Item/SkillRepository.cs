10746ba9e67dc658029ab4437c824e841d3d5e2c
SeongBin0516 <rkfmskvos@gmail.com>
2024-09-30 16:54:50 +0900

    Add SkillRepositiry
    스킬 목록 추가

--- 26TextRPG/26TextRPG/Item/SkillRepository.cs
+++ 26TextRPG/26TextRPG/Item/SkillRepository.cs
@@ -21,17 +21,29 @@ namespace _26TextRPG
 			//Reference 1:공격력, 2:방어력, 3:최대체력, 4:최대마나 5:속도
 			Warrior = new List<Skill>() // 전사 스킬 리스트
             {
-				new("배쉬",3,1,1.5,false),
+				new Skill("배쉬",3,1,1.5,false),
+				new Skill("슬래쉬", 5, 1, 2.0f, false),
+				new Skill("방패 밀치기", 4, 2, 1.5f, false),
+				new Skill("원드밀", 8, 1, 1.5f, true),
+				new Skill("파이널 히트", 10, 3, 1.5f, false),
 				//스킬 추가
 			};
 			Archer = new List<Skill>() // 궁수 스킬 리스트
             {
-				new("해드샷",3,1,1.5,false),
+				new Skill("해드샷", 3, 1, 1.5f, false),
+				new Skill("연속 사격", 5, 1, 2.0f, false), 
+				new Skill("다중 화살", 5, 1, 1.3f, true), 
+				new Skill("신속 발사", 3, 5, 2.0f, false), 
+				new Skill("파이널 샷", 10, 1, 3.0f, false), 
                 //스킬 추가
             };
 			Wizard = new List<Skill>()  // 마법사 스킬 리스트
             {
-				new("매직미사일",3,1,1.5,false),
+				new Skill("매직 미사일", 4, 5, 0.5f, false), 
+				new Skill("매직 에로우", 6, 5, 0.8f, false), 
+				new Skill("매직 스피어", 3, 5, 1.1f, false), 
+				new Skill("체인 라이트닝", 7, 5, 1.6f, true),
+				new Skill("파이어 스톰", 10, 5, 2.0f, true), 
                 //스킬 추가
             };
 			AllSkills = new List<Skill>();

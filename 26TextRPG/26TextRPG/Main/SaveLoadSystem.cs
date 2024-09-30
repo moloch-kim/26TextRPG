5e1315d31ab2786fef321613f787b95fc660a49a
geuneda <whdrmsvjzl@naver.com>
2024-09-30 15:47:35 +0900

    fix: Resolve Json deserialization issue add TypeNameHandling
    -Item 클래스의 직렬화 오류 해결
    - TypeNameHandling 설정을 사용하여 다형성 지원

--- 26TextRPG/26TextRPG/Main/SaveLoadSystem.cs
+++ 26TextRPG/26TextRPG/Main/SaveLoadSystem.cs
@@ -10,7 +10,11 @@ namespace _26TextRPG.Main
         static MainScene mainScene = new MainScene();
         public static void SaveGame(Player player)
         {
-            string json = JsonConvert.SerializeObject(player, Formatting.Indented);
+            JsonSerializerSettings settings = new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Auto
+            };
+            string json = JsonConvert.SerializeObject(player, Formatting.Indented, settings);
             File.WriteAllText(filePath, json);
         }
         public static Player LoadGame()
@@ -18,13 +22,17 @@ namespace _26TextRPG.Main
             if (File.Exists(filePath))
             {
                 string json = File.ReadAllText(filePath);
-                Player player = JsonConvert.DeserializeObject<Player>(json);
+                JsonSerializerSettings settings = new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.Auto
+                };
+                Player player = JsonConvert.DeserializeObject<Player>(json, settings);
                 return player;
             }
             else
             {
                 Console.WriteLine("불러올 파일이 없습니다.");
-                Thread.Sleep(3000);
+                Thread.Sleep(1500);
                 return null;
                 mainScene.CreatePlayer();
             }

0a841a08743472de064609b871d9bda57ac8e213
SeongBin0516 <rkfmskvos@gmail.com>
2024-09-30 16:19:04 +0900

    Update Skill
    스킬과 플래이어에 참조변수 추가

--- 26TextRPG/26TextRPG/Skill.cs
+++ 26TextRPG/26TextRPG/Skill.cs
@@ -2,10 +2,11 @@ public class Skill
 {
     public string Name { get; } //스킬 이름
     public int ManaCost { get; } //마나 소모량
+    public int reference { get; } //참조 변수
     public float Multiplier { get; } //스킬 배율
     public bool IsArea { get; }  //  true 면 광역, false 면 단일
 
-    public Skill(string name, int manaCost, float multiplier, bool isArea)
+    public Skill(string name, int manaCost,int reference, float multiplier, bool isArea)
     {
         Name = name;
         ManaCost = manaCost;

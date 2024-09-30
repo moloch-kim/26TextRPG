268604201d8ee4db8a3f70d9a44de3737cd32340
SeongBin0516 <rkfmskvos@gmail.com>
2024-09-30 19:09:22 +0900

    Update skill price
    스킬에 가격 추가

--- 26TextRPG/26TextRPG/Skill.cs
+++ 26TextRPG/26TextRPG/Skill.cs
@@ -5,13 +5,15 @@ public class Skill
     public int Reference { get; } //참조 변수 1:공격력, 2:방어력, 3:최대체력, 4:최대마나 5:속도
     public float Multiplier { get; } //스킬 배율
     public bool IsArea { get; }  //  true 면 광역, false 면 단일
+    public int Price { get; } // 가치
 
-    public Skill(string name, int manaCost,int reference, float multiplier, bool isArea)
+    public Skill(string name, int manaCost,int reference, float multiplier, bool isArea, int price)
     {
         Name = name;
         ManaCost = manaCost;
         Reference = reference;
         Multiplier = multiplier;
         IsArea = isArea;
+        Price = price;
     }
 }

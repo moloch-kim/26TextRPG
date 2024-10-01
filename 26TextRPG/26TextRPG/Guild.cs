137a79f66ce6f433a27679d7fd5d3de32b3b684d
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-10-01 13:01:52 +0900

    Update Guild and Quests

--- 26TextRPG/26TextRPG/Guild.cs
+++ 26TextRPG/26TextRPG/Guild.cs
@@ -1,38 +1,126 @@
-﻿namespace _26TextRPG
+﻿using _26TextRPG.Main;
+
+namespace _26TextRPG
 {
     public class Guild
     {
-        private List<Quest> availableQuests = new List<Quest>();
-
+        public List<Quest> QuestList = new List<Quest>();
+        MainScene mainScene = new MainScene();
         public Guild()
         {
             // 예시 퀘스트 추가
-            availableQuests.Add(new Quest("오크 죽이기", "오크를 5마리 처치하시오.", "오크", 5, 1000, 100, null));
-            
+            QuestList.Add(new Quest("고블린 죽이기", "오크를 5마리 처치하시오.", "오크", 5, 1000, 100, ItemRepository.GetRandomWeapon()));
         }
 
-        public void ShowAvailableQuests()
+        public void DisplayQuests()
         {
-            Console.WriteLine("길드에서 수주할 수 있는 퀘스트:");
-            foreach (var quest in availableQuests)
+            Console.WriteLine("길드에서 수주할 수 있는 의뢰:");
+            Console.WriteLine("--------------------------------------------------------------------");
+            for (int i = 0; i < QuestList.Count; i++)
             {
-                Console.WriteLine($"- {quest.Name}: {quest.Description}");
+                var item = QuestList[i];
+
+                if (item == null)
+                {
+                    Console.WriteLine($"디버그: QuestList[{i}]가 null입니다.");
+                    continue;
+                }
+                switch (QuestList[i].IsCommissioned)
+                {
+                    case true:
+                        if (!QuestList[i].IsCompleted)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.WriteLine($"{i + 1}.  {QuestList[i].Name} : {QuestList[i].Description} - 수주됨");
+                            Console.ResetColor(); 
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            Console.WriteLine($"{i + 1}.  {QuestList[i].Name} : {QuestList[i].Description} - 완료");
+                            Console.ResetColor();
+                        }
+                        break;
+                    case false:
+                        Console.WriteLine($"{i + 1}.  {QuestList[i].Name} : {QuestList[i].Description} ");
+                        break;
+                }
+            }
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine();
+            //foreach (var quest in availableQuests)
+            //{
+            //    Console.WriteLine($"- {quest.Name}: {quest.Description}");
+            //}
+        }
+        public void ObtainQuests()
+        {
+            Player playerData = Player.Instance;
+            Console.Clear();
+            mainScene.TypingEffect("길드에 입장했습니다!", 40);
+            bool InShop = true;
+            while (InShop)
+            {
+
+                Console.WriteLine();
+                Console.WriteLine("--------------------------------------------------------------------");
+                DisplayQuests();
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine("나가기 : P ");
+                mainScene.TypingEffect("수주할 의뢰의 번호를 입력하세요. (취소하려면 0 입력):", 40);
+                Console.WriteLine(); Thread.Sleep(100);
+                string input = Console.ReadLine();
+                if (input == "p" || input == "P")
+                {
+                    InShop = false;
+                    mainScene.TypingEffect("길드에서 나갔습니다.", 40);
+                    Console.WriteLine(); Thread.Sleep(100);
+                }
+                else if (int.TryParse(input, out int choice))
+                {
+                    if (choice == 0)
+                    {
+                        mainScene.TypingEffect("수주를 취소했습니다.", 40);
+                        Console.WriteLine(); Thread.Sleep(100);
+                        return;
+                    }
+
+                    if (choice > 0 && choice <= QuestList.Count)
+                    {
+                        Quest selectedQuest = QuestList[choice - 1];
+                        playerData.Quest.Add(selectedQuest);
+                        selectedQuest.IsCommissioned = true; 
+                        mainScene.TypingEffect($"{selectedQuest.Name}을(를) 수주하여 퀘스트에 추가했습니다.", 40);
+                        Console.WriteLine();
+                        Thread.Sleep(100);
+                    }
+                    else
+                    {
+                        mainScene.TypingEffect("잘못된 선택입니다.", 40);
+                        Console.WriteLine(); Thread.Sleep(100);
+                    }
+                }
+                else
+                {
+                    mainScene.TypingEffect("숫자를 입력해주세요.", 40);
+                    Console.WriteLine(); Thread.Sleep(100);
+                }
+                Console.Clear();
             }
         }
-
-        public void CompleteQuest(Player player)
+        public void CompleteQuest()
         {
-            Quest quest = player.Quest;
+            Player playerData = Player.Instance;
+            for (int i = 0; i < playerData.Quest.Count; i++)
+            {
+                if (playerData.Quest[i].IsCompleted)
+                {
+                    Console.WriteLine($"[{playerData.Quest[i].Name}] 의뢰가 완료되었습니다.");
+                }
+                else
+                {
 
-            if (quest.IsCompleted)
-            {
-                quest.CompleteQuest(player);
-                availableQuests.Remove(quest);
-                Console.WriteLine($"{quest.Name} 퀘스트가 완료되었습니다.");
-            }
-            else
-            {
-                Console.WriteLine($"{quest.Name} 퀘스트는 아직 완료되지 않았습니다.");
+                }
             }
         }
     }
--- 26TextRPG/26TextRPG/Item/Potion.cs
+++ 26TextRPG/26TextRPG/Item/Potion.cs
@@ -41,18 +41,22 @@ namespace _26TextRPG
                     Console.WriteLine($"{Name}을(를) 사용하여 체력이 {Effect}만큼 회복되었습니다.");
                     break;
                 case 2: //공격력 강화 포션
-                    playerData.AttackPower += Effect; 
+                    playerData.AttackPower += Effect;
+                    Console.WriteLine($"{Name}을(를) 사용하여 공격력이 {Effect}만큼 상승했습니다!");
                     break;
                 case 3: //방어력 강화 포션
                     playerData.DefensePower += Effect;
+                    Console.WriteLine($"{Name}을(를) 사용하여 방어력이 {Effect}만큼 상승했습니다!");
                     break;
                 case 4: //속도 강화 포션
                     playerData.Speed += Effect;
+                    Console.WriteLine($"{Name}을(를) 사용하여 속도가 {Effect}만큼 상승했습니다!");
                     break;
                 case 5: //전체 능력치 강화 포션
                     playerData.AttackPower += Effect;
                     playerData.DefensePower += Effect;
                     playerData.Speed += Effect;
+                    Console.WriteLine($"{Name}을(를) 사용하여 모든 능력치가 {Effect}만큼 상승했습니다!!");
                     break;
             }
             playerData.Inventory.Remove(this);
